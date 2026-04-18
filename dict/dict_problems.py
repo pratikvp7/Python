fcbcff2a00af65edb122363d1a147ca9014f4ca7
pratikvp7 <168306167+pratikvp7@users.noreply.github.com>
2026-04-18 21:42:40 +0530

    dict

---
+++ dict/dict_problems.py
@@ -0,0 +1,27 @@
+
+
+class DictProblems:
+
+
+    def dict_operations(self):
+        dict1 = {
+            1: 'abc',
+            2: 'def',
+            3: 'ghi'
+        }
+        print(dict1)
+        for key, value in dict1.items():
+            print(key, value)
+
+        keys = dict1.keys()
+        values = dict1.values()
+        print(keys, values)
+
+        list(values).insert(1, 'jkl')
+        print(dict1.values())
+
+
+
+if __name__ == '__main__':
+    dict = DictProblems()
+    dict.dict_operations()
